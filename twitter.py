78732fec8baea6d3b740b43abd5e9a6178c1f75c
unknown <nfranceshealy@gmail.com>
2018-03-20 19:56:59 +0000

    twitter

--- twitter.py
+++ twitter.py
@@ -1,4 +1,5 @@
 import tweepy
+import requests
 
 auth = tweepy.OAuthHandler("NhwEslehY264mPa0owS6i4BZU","zzuVEQsOSOovoHXZ965Me4HKlt0VMiH87ruztaTo71cPH9VIYK")
 auth.set_access_token("2282751926-Avzs0JEIg0WIePxJO7y0meUYlQj2gR8uMfrnF6q","ePKPPikAAChk6W5dNnQPLTUOcOxNBzGHknI9DpO9aQXig")
